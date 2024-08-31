01fbb90d619b91fafb8af826d1e1adc6c167f585
yeferson <siryefers@gmail.com>
2024-08-31 10:57:17 +0000

    auto

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ if not cap.isOpened():
 # Obtener el número total de cuadros en el video
 total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
-frame_interval = 6  # Leer cada 30 cuadros
+frame_interval = 5  # Leer cada 30 cuadros
 frame_count = 0
 
 numeros_pedidos = []
